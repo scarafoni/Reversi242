5c5143a65b87b8a3409b5cd55e5e5dffba00689f
danscarafoni <dan@danUbuntu.(none)>
2014-01-31 15:33:09 -0500

    sample client should compile universally now

--- SampleClient.java
+++ SampleClient.java
@@ -6,12 +6,12 @@ public class SampleClient {
         String hostName = "localhost";
         int portNumber = 4444;
 
-        try (
+        try {
             Socket kkSocket = new Socket(hostName, portNumber);
             PrintWriter out = new PrintWriter(kkSocket.getOutputStream(), true);
             BufferedReader in = new BufferedReader(
-                new InputStreamReader(kkSocket.getInputStream()));)
-        {
+                new InputStreamReader(kkSocket.getInputStream()));
+        
             BufferedReader stdIn =
                 new BufferedReader(new InputStreamReader(System.in));
             String fromServer;
@@ -27,10 +27,7 @@ public class SampleClient {
                     System.out.println("Client: " + fromUser);
                     out.println(fromUser);
                 }
-            }
-        } catch (UnknownHostException e) {
-            System.err.println("Don't know about host " + hostName);
-            System.exit(1);
+          }  
         } catch (IOException e) {
             System.err.println("Couldn't get I/O for the connection to " +
                 hostName);
