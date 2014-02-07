7213a1560dba86c7238faa99ee0236881918c672
scarafoni <dscarafo@u.rochester.edu>
2014-02-07 00:37:43 -0500

    changes to run shell

--- runGame.sh
+++ runGame.sh
@@ -2,22 +2,12 @@
 # Use: run-example player1 player2
 
 # Clean out the FIFOS and processes upon exit
-trap '/bin/rm player1in player1out player2in player2out ; kill $(jobs -p)' EXIT
-p1="java SampleClient 4444"
-p2="java SampleClient 5555"
+#trap '/bin/rm player1in player1out player2in player2out ; kill $(jobs -p)' EXIT
+#p1="java SampleClient 4444"
+#p2="java SampleClient 5555"
 # Create FIFOs for player input and output.
 # A FIFO looks like a file to other programs.
-#mkfifo player1in
-#mkfifo player1out
-#mkfifo player2in
-#mkfifo player2out
 
-#touch player1in
-#touch player1out
-#touch player2in
-#touch player2out
-
-# Start the player processes
 #($p1) < player1in & #> player1out &
 #($p2) < player2in > player2out &
 
@@ -27,4 +17,4 @@ p2="java SampleClient 5555"
 # for too long, the read times out and the game is over.
 # When the game is over, the gui program exits, ending this script and
 # cleaning up the player processes and FIFOs.
-java Reversi 50000 text computer computer player1in player1out player2in player2out
+java Reversi 50000 text computer computer "$1" "$2"
